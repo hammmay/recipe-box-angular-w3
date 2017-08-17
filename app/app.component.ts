edf6ebd3b3f60d7be1096d66a0bb19c6e394fa3c
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-17 10:20:34 -0700

    Add additional attributes and copy to recipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
     <h3>{{tagline}}</h3>
     <ul>
       <li *ngFor="let currentRecipe of recipes">{{currentRecipe.title}}</li>
+
     </ul>
   </div>
   `
@@ -16,13 +17,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   tagline: string = 'A Collection of Delicious';
   recipes: Recipe[] = [
-    new Recipe('Lazy Sunday Pasta'),
-    new Recipe('Easy Lentil Soup'),
-    new Recipe('Protein Pancakes')
+    new Recipe('Lazy Sunday Pasta','Ingredients: noodles, peas, mint, basil, parmesan, cream','Directions: 1. Boil the pasta 2. Saute peas with parmesan and cream 3. Add pasta and herbs to pea mixure and mix'),
+    new Recipe('Easy Lentil Soup','lentils, crushed tomatoes, garlic, celery, carrots, onion','1. Saute all veggies 2. Add tomatoes, water, lentils, and boil for an hour')
   ];
 
 }
 
 export class Recipe {
-  constructor(public title: string) { }
+  constructor(public title: string, public ingredients: string, public directions: string) { }
 }
